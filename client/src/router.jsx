b306986dbc96a8c4d4c6e9da92918385e5763013
iamrayghazali <151096230+iamrayghazali@users.noreply.github.com>
2025-03-26 01:19:31 +0100

    Added routeing

---
+++ client/src/router.jsx
@@ -0,0 +1,27 @@
+import {createBrowserRouter} from "react-router-dom";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import ErrorElement from "./pages/ErrorElement";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />,  // Starting page, maybe a welcome screen or map
+        errorElement: <ErrorElement />,
+    },
+    {
+        path: "/register",
+        element: <Register />,
+    },
+    {
+        path: "/login",
+        element: <Login />,
+    },
+    {
+        path: "*",  // Catch-all route for unknown pages
+        element: <ErrorElement />,
+    },
+]);
+
+export default router;
